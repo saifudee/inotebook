833888d55fd377ea7df4e6890bda3eb677cecaaa
Saifdeen <saifudeen0323@gmail.com>
2022-03-04 19:14:36 +0530

    56

--- inotebook - backend/routes/auth.js
+++ inotebook - backend/routes/auth.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const User = require('../models/User')
 const router = express.Router()
+const { body, validationResult } = require('express-validator');
 
-router.get('/',(req,res)=>{
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
+router.get('/',[
+    body('name',"Enter a vaild name").isLength({ min: 3 }),
+    body('email',"Enter a vaild email").isEmail(),
+    body('password',"Enter a passowrd with min length 5 characters").isLength({ min: 5 }),
+],(req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+      }).then(user => res.json(user))
+      .catch(err=>{console.log(err)
+        res.json({error:"Please enter a unique value for email",message:err.message})
+      })
 })
 
 module.exports=router

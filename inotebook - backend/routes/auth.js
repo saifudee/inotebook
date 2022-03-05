1baf7f1f63c3c75648e320adabf679426ec8d52f
Saifdeen <saifudeen0323@gmail.com>
2022-03-05 19:09:45 +0530

    122

--- inotebook - backend/routes/auth.js
+++ inotebook - backend/routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express')
 const User = require('../models/User')
 const router = express.Router()
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs')
 
 router.post('/createuser', [
   body('name', "Enter a vaild name").isLength({ min: 3 }),
@@ -17,9 +18,11 @@ router.post('/createuser', [
     if (user) {
       return res.status(400).json({ error: "Sorry this email is already exists" })
     }
+    const salt = await bcrypt.genSalt(10)
+    const secPass = await bcrypt.hash(req.body.password,salt)
     user = await User.create({
       name: req.body.name,
-      password: req.body.password,
+      password: secPass,
       email: req.body.email,
     })
     res.json(user)

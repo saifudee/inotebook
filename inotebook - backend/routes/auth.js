0605a00f73a6d3ef48265732adfa48fad3a8888d
Saifdeen <saifudeen0323@gmail.com>
2022-03-05 19:11:58 +0530

    1

--- inotebook - backend/routes/auth.js
+++ inotebook - backend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express')
 const User = require('../models/User')
 const router = express.Router()
 const { body, validationResult } = require('express-validator');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
 
 router.post('/createuser', [
   body('name', "Enter a vaild name").isLength({ min: 3 }),

7699d1ee02be6e5b3e2ec64cfbdec8e71682ed15
Saifdeen <saifudeen0323@gmail.com>
2022-03-05 20:09:00 +0530

    1238

--- inotebook - backend/routes/auth.js
+++ inotebook - backend/routes/auth.js
@@ -3,7 +3,10 @@ const User = require('../models/User')
 const router = express.Router()
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
+const JWT_SERECT = "Hello"
+// CreateUser
 router.post('/createuser', [
   body('name', "Enter a vaild name").isLength({ min: 3 }),
   body('email', "Enter a vaild email").isEmail(),
@@ -18,18 +21,54 @@ router.post('/createuser', [
     if (user) {
       return res.status(400).json({ error: "Sorry this email is already exists" })
     }
-    const salt = await bcrypt.genSalt(10)
+    const salt = await bcrypt.genSalt(10);
     const secPass = await bcrypt.hash(req.body.password,salt)
     user = await User.create({
       name: req.body.name,
       password: secPass,
       email: req.body.email,
     })
-    res.json(user)
+    const data = {
+      user:{
+        id:user.id
+      }
+    }
+    const authToken = jwt.sign(data,JWT_SERECT);
+    res.json({authToken})
   } catch (error) {
     console.log(error.message);
-    res.status(400).send({ error: "Some error occured" })
+    res.status(400).send({ error: "Internal Server Error" })
   }
 })
-
+// Authentication login 
+router.post('/loginuser', [
+  body('email', "Enter a vaild email").isEmail(),
+  body('password', "Password cant be blank").exists(),
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const {email,password}=req.body
+  try {
+    let user = await User.findOne({email});
+    if(!user){
+    return res.status(400).json({ error: "Invalid email/password" });
+    }
+    const comparePassword = await bcrypt.compare(password,user.password)
+    if(!comparePassword){
+    return res.status(400).json({ error: "Invalid email/password" });
+    }
+    const data = {
+      user:{
+        id:user.id
+      }
+    }
+    const authToken = jwt.sign(data,JWT_SERECT);
+    res.json({authToken})
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).send({ error: "Internal Server Error" })
+  }
+})
 module.exports = router

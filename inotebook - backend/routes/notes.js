4b3265c82c98b575306b95434e21c78714303981
Saifdeen <saifudeen0323@gmail.com>
2022-03-06 13:26:29 +0530

    112

--- inotebook - backend/routes/notes.js
+++ inotebook - backend/routes/notes.js
@@ -34,5 +34,34 @@ router.post('/addnote',fetchuser, [
     console.log(error.message);
     res.status(400).send({ error: "Internal Server Error" })   
 }
-  })
+})
+router.put('/updatenote/:id',fetchuser, async (req, res) => { 
+    const {title,description,tag} = req.body
+    const newNote = {}
+        if(title){newNote.title=title};
+        if(description){newNote.description=description};
+        if(tag){newNote.tag=tag};
+    let notes = await Notes.findById(req.params.id);
+    if(!notes){
+      return res.status(404).send("Not Found");
+    }
+    if(notes.user.toString() !== req.user.id ){
+      return res.status(401).send("Not Allowed");
+    }
+    notes = await Notes.findByIdAndUpdate(req.params.id,{$set:newNote},{newNote:true});
+    res.json({notes});
+})
+router.delete('/deletenote/:id',fetchuser, async (req, res) => { 
+  let notes = await Notes.findById(req.params.id);
+  if(!notes){
+    return res.status(404).send("Not Found");
+  }
+  if(notes.user.toString() !== req.user.id ){
+    return res.status(401).send("Not Allowed");
+  }
+  notes = await Notes.findByIdAndDelete(req.params.id);
+  res.json({"Success":"Your Note has been deleted",notes:notes});
+})
+
+
 module.exports=router

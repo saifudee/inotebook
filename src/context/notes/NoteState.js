8f6f62d5e83c301bd85f5beb6e9f3e9dcd18658e
Saifdeen <saifudeen0323@gmail.com>
2022-03-10 10:20:57 +0530

    12434

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -3,93 +3,67 @@ import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props)=>{
-  const initialNotes= [
-  {
-    "_id": "62243341d5392c17a59276216",
-    "user": "62236ca5f3c150792ae34345",
-    "title": "my title",
-    "description": "msnfkendfw",
-    "tag": "general",
-    "date": "2022-03-06T04:10:05.480Z",
-    "__v": 0
-  },
-  {
-    "_id": "6224341d51392c17a59276218",
-    "user": "62236ca5f3c150792ae34345",
-    "title": "my title",
-    "description": "msnfkendfw",
-    "tag": "general",
-    "date": "2022-03-06T04:10:05.709Z",
-    "__v": 0
-  },
-  {
-    "_id": "6224341d5392c17a59276221a",
-    "user": "62236ca5f3c150792ae34345",
-    "title": "my title",
-    "description": "msnfkendfw",
-    "tag": "general",
-    "date": "2022-03-06T04:10:05.997Z",
-    "__v": 0
-  },
-  {
-    "_id": "6224341e53922c17a5927621c",
-    "user": "62236ca5f3c150792ae34345",
-    "title": "my title",
-    "description": "msnfkendfw",
-    "tag": "general",
-    "date": "2022-03-06T04:10:06.169Z",
-    "__v": 0
-  },
-  {
-    "_id": "6224341e5392c127a5927621e",
-    "user": "62236ca5f3c150792ae34345",
-    "title": "my title",
-    "description": "msnfkendfw",
-    "tag": "general",
-    "date": "2022-03-06T04:10:06.352Z",
-    "__v": 0
-  },
-  {
-    "_id": "6224341e5392c127a59276220",
-    "user": "62236ca5f3c150792ae34345",
-    "title": "my title",
-    "description": "msnfkendfw",
-    "tag": "general",
-    "date": "2022-03-06T04:10:06.567Z",
-    "__v": 0
-  },
-  {
-    "_id": "62245873dbb937387e3314ad",
-    "user": "62236ca5f3c150792ae34345",
-    "title": "my title",
-    "description": "msnfkendfw",
-    "tag": "general",
-    "date": "2022-03-06T06:45:07.094Z",
-    "__v": 0
-  }
-]
+  const host = "http://localhost:5000/"
+  const initialNotes =[]
 const [notes,setNotes]=useState(initialNotes)
-const addNote = (title,description,tag)=>{
+
+const getNotes = async()=>{
+  const response = await fetch(`${host}api/notes/getalluser`,{
+    method:'GET',
+    headers:{
+      'Content-Type':"application/json",
+      "auth-token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjIyMzZjYTVmM2MxNTA3OTJhZTM0MzQ1In0sImlhdCI6MTY0NjQ5NTAyMX0.HVoNJWfOt5NnDZ398Ycnhn8NbvCJPJKKOqFldR5FyeY"
+    },
+  })
+  const json = await response.json();
+  console.log(json)
+  setNotes(json)
+}
+const addNote = async(title,description,tag)=>{
+  const response = await fetch(`${host}/api/notes/addnote`,{
+    method:'POST',
+    headers:{
+      "Content-Type":"application/json",
+      "auth-token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjIyMzZjYTVmM2MxNTA3OTJhZTM0MzQ1In0sImlhdCI6MTY0NjQ5NTAyMX0.HVoNJWfOt5NnDZ398Ycnhn8NbvCJPJKKOqFldR5FyeY"
+    },
+    body:JSON.stringify({title,description,tag})
+  })
  const note={
-    "_id": "6224341d5392c17aq592762164",
-    "user": "62236ca5f3c150792ae34345",
-    "title":title,
-    "description":description,
-    "tag":tag,
-    "date": "2022-03-06T04:10:05.480Z",
-    "__v": 0
-  };
+  "user": "62236ca5f3c150792ae34342",
+  "title":title,
+  "description":description,
+  "tag":tag,
+  "_id": "622973fcb2c6b427c379a133e",
+  "date": "2022-03-10T03:43:56.373Z",
+  "__v": 0
+}
   setNotes(notes.concat(note))
 }
-const editNote = ()=>{
-  
+const editNote = async(id,title,description,tag)=>{
+  const response = await fetch(`${host}/api/notes/updatenote/${id}`,{
+    method:'POST',
+    headers:{
+      'Content-Type':'application/json',
+      'auth-token':"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjIyMzZjYTVmM2MxNTA3OTJhZTM0MzQ1In0sImlhdCI6MTY0NjQ5NTAyMX0.HVoNJWfOt5NnDZ398Ycnhn8NbvCJPJKKOqFldR5FyeY"
+    },
+    body:JSON.stringify({title,description,tag})
+  })
+  const json = response.json();
+  for (let index = 0; index <notes.length; index++) {
+    const element = notes[index];
+    if(element._id === id){
+      element.title = title
+      element.description= description
+      element.tag = tag
+    }
+  }
 }
 const deleteNote = (id)=>{
   const newNotes = notes.filter((notes)=>{return notes._id!==id})
   setNotes(newNotes)
 }
     return(
-        <NoteContext.Provider value={{notes,addNote,editNote,deleteNote }}>
+        <NoteContext.Provider value={{notes,addNote,editNote,deleteNote,getNotes}}>
         {props.children}
         </NoteContext.Provider>
     )
